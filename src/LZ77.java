4dd377316c142578d75b34cb0f6ac6c4e8753b61
Martin Helmersen <Marhelmersen@gmail.com>
2021-11-02 13:21:26 +0100

    initial commit

---
+++ src/LZ77.java
@@ -0,0 +1,13 @@
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+
+/**
+ * Class for compression with Lempel-Ziv
+ */
+public class LZ77 {
+
+
+
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+}
